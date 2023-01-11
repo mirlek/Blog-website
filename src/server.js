4a2b4470be050eb947c00e6ff0284a41f413f35d
mirlek <mirlenko.ekat@gmail.com>
2023-01-11 13:53:48 +0100

    MongoDb is added

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { MongoClient } from "mongodb";
 
 let articlesInfo = [{
     name: 'learn-react',
@@ -25,6 +26,22 @@ app.use(express.json());            //middleware, very important for getting ans
 //     const { name } = req.params;    // same is const name = req.params.name
 //     res.send(`Hello ${name} ?!`);
 // });
+
+app.get('/api/articles/:name', async(req, res) => {             ///connection to the MongoDb
+    const { name } = req.params;
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
+
+    const db = client.db('react-blog-db');                 ///react-blog-db is a name of the database
+    
+    const article = await db.collection('articles').findOne({name});
+
+    if (article) {
+        res.json(article);
+    } else {
+        res.sendStatus(404);
+    };
+});
 
 app.put('/api/articles/:name/upvote', (req, res) => {
     const { name } = req.params;
